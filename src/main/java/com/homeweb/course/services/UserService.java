8e5de4faa761760c942d3ec87ad222c060d84d57
elaniacs <carensousaesilva@gmail.com>
2022-07-01 09:49:49 -0300

    Service layer, component registration

---
+++ src/main/java/com/homeweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.homeweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.homeweb.course.entities.User;
+import com.homeweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); 
+	}
+}
